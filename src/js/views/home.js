efe45c2747ae8298d4521361bdb2dcb65d900914
Mar 🌊 <mariajara929@gmail.com>
2025-01-17 15:26:36 +0000

    Inicial Commit

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,32 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	return (
+		<div className="text-center mt-5">
+			<ul className="list-group">
+				<li className="list-group-item d-flex">
+					<div className="">
+						<img />
+					</div>
+					<div className="">
+						<ul>
+							<li>Nombre</li>
+							<li>Address</li>
+							<li>Phone Number</li>
+							<li>E-mail</li>
+						</ul>
+					</div>
+					<div className="">
+						<button className="btn">
+							<i className="fa-solid fa-pen-to-square"></i>
+						</button>
+						<button className="btn">
+							<i class="fa-solid fa-trash"></i>
+						</button>
+					</div>
+				</li>
+			</ul>
+		</div>
+	);
+};
